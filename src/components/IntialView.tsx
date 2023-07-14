bd8bc0714e4a663e1bb8bac80548414deef53889
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-07-14 15:52:11 -0300

    retocando

--- src/components/IntialView.tsx
+++ src/components/IntialView.tsx
@@ -1,14 +1,37 @@
+import { useState, useEffect } from "react";
 import featuresCards from "../features.json";
 import { Card, Container, Row, Col } from "react-bootstrap";
 import LoginAuth0 from "./LoginAuth0";
 import "../styles/IntialView.css";
 
 const IntialView = () => {
+  const [isXsScreen, setIsXsScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsXsScreen(window.innerWidth <= 990);
+    };
+
+    handleResize(); // Verificar el tamaño de la pantalla al cargar la página
+
+    window.addEventListener("resize", handleResize); // Escuchar cambios en el tamaño de la pantalla
+
+    return () => {
+      window.removeEventListener("resize", handleResize); // Limpiar el evento al desmontar el componente
+    };
+  }, []);
+
   return (
     <>
-      <Row className="d-flex">
+      <Row
+        className="d-flex"
+        style={{ flexDirection: isXsScreen ? "column-reverse" : "column" }}
+      >
         <Col>
-          <Container className="d-flex mt-5" fluid>
+          <Container
+            className="d-flex mt-5"
+            style={{ maxWidth: isXsScreen ? "" : "100%" }}
+          >
             <div className="d-flex justify-content-center mx-auto align-items-center flex-wrap mt-5">
               {featuresCards.map((feature) => (
                 <Col
@@ -19,7 +42,9 @@ const IntialView = () => {
                   key={feature.id}
                 >
                   <Card
-                    className="zoom mx-4 bg-dark text-white"
+                    className={`mx-4 bg-dark text-white ${
+                      isXsScreen ? "" : "zoom"
+                    }`}
                     style={{ overflow: "hidden" }}
                   >
                     <Card.Header>{feature.title}</Card.Header>
