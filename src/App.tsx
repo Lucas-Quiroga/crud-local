71e761307c9180b4b15bd24bbe5b94156c67014a
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-07-14 18:56:14 -0300

    again

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AppMain from "./components/AppMain";
-import IntialView from "./components/IntialView";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import AppMain from "./components/AppMain";
+// import IntialView from "./components/IntialView";
+import LoginAuth0 from "./components/LoginAuth0";
+import LogoutAuth0 from "./components/LogoutAuth0";
 import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { user } = useAuth0();
   return (
-    <BrowserRouter>
-      <IntialView />
-      <Routes>
-        {/* <Route path="/" element={<IntialView />} /> */}
+    <div>
+      <LoginAuth0 />
+      <LogoutAuth0 />
 
-        {/* <Route path="/login" element={<LoginAuth0 />} /> */}
-        <Route path="/todo/*" element={<AppMain />} />
-      </Routes>
-    </BrowserRouter>
+      <div className="profile">{user?.email}</div>
+    </div>
+    // <BrowserRouter>
+    //   <IntialView />
+    //   <Routes>
+    //     {/* <Route path="/" element={<IntialView />} /> */}
+
+    //     {/* <Route path="/login" element={<LoginAuth0 />} /> */}
+    //     <Route path="/todo/*" element={<AppMain />} />
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
