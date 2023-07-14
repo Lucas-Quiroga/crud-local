39adbce46bf227d40284825f64ce8aa60c97ab0d
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-07-14 18:58:34 -0300

    again

--- src/components/LoginAuth0.tsx
+++ src/components/LoginAuth0.tsx
@@ -2,16 +2,14 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Button, Container } from "react-bootstrap";
 
 const LoginAuth0 = () => {
-  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
 
   return (
-    !isAuthenticated && (
-      <Container className="d-flex justify-content-center mx-auto align-items-center">
-        <Button onClick={() => loginWithRedirect()} variant="light">
-          Sign In
-        </Button>
-      </Container>
-    )
+    <Container className="d-flex justify-content-center mx-auto align-items-center">
+      <Button onClick={() => loginWithRedirect()} variant="light">
+        Sign In
+      </Button>
+    </Container>
   );
 };
 
--- src/components/LogoutAuth0.tsx
+++ src/components/LogoutAuth0.tsx
@@ -2,19 +2,17 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "react-bootstrap";
 
 const LogoutAuth0 = () => {
-  const { logout, isAuthenticated } = useAuth0();
+  const { logout } = useAuth0();
   return (
-    isAuthenticated && (
-      <Button
-        variant="secondary"
-        className="mt-5 justify-content-end"
-        onClick={() =>
-          logout({ logoutParams: { returnTo: window.location.origin } })
-        }
-      >
-        Log Out
-      </Button>
-    )
+    <Button
+      variant="secondary"
+      className="mt-5 justify-content-end"
+      onClick={() =>
+        logout({ logoutParams: { returnTo: window.location.origin } })
+      }
+    >
+      Log Out
+    </Button>
   );
 };
 
