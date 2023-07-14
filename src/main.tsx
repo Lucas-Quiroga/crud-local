175aedece9f62827d7b9d8a8df29ca8b16b272ea
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-07-14 18:48:57 -0300

    again

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: import.meta.env.VITE_APPDEPLOY + "/todo",
+        redirect_uri: import.meta.env.VITE_APPDEPLOY,
       }}
     >
       <UsersContextProvider>
