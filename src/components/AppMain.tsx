a440d7cc07141d766e3c4bf80ad8e3a0db5ffe5f
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-07-14 17:09:47 -0300

    again

--- src/components/AppMain.tsx
+++ src/components/AppMain.tsx
@@ -8,58 +8,64 @@ import LogoutAuth0 from "./LogoutAuth0";
 import Spinner from "react-bootstrap/Spinner";
 
 const AppMain = () => {
-  const { isLoading } = useAuth0();
+  const { isLoading, isAuthenticated } = useAuth0();
 
   return (
-    <Container className="d-flex flex-column justify-content-center mx-auto vh-100">
-      <Row
-        style={{
-          borderRadius: "1rem",
-          boxSizing: "border-box",
-          overflow: "hidden",
-        }}
-        className="d-flex justify-content-center mb-2"
-      >
-        {isLoading ? (
-          <Spinner
-            animation="border"
-            role="status"
-            className="d-flex flex-column justify-content-center mx-auto align-items-center"
+    <>
+      {isAuthenticated ? (
+        <Container className="d-flex flex-column justify-content-center mx-auto vh-100">
+          <Row
+            style={{
+              borderRadius: "1rem",
+              boxSizing: "border-box",
+              overflow: "hidden",
+            }}
+            className="d-flex justify-content-center mb-2"
           >
-            <span className="visually-hidden">Loading...</span>
-          </Spinner>
-        ) : (
-          <>
-            <Col
-              lg={2}
-              style={{ backgroundColor: " #e3e9eb " }}
-              className="d-flex flex-column"
-            >
-              <UserInfo />
+            {isLoading ? (
+              <Spinner
+                animation="border"
+                role="status"
+                className="d-flex flex-column justify-content-center mx-auto align-items-center"
+              >
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            ) : (
+              <>
+                <Col
+                  lg={2}
+                  style={{ backgroundColor: " #e3e9eb " }}
+                  className="d-flex flex-column"
+                >
+                  <UserInfo />
 
-              <LogoutAuth0 />
-            </Col>
+                  <LogoutAuth0 />
+                </Col>
 
-            <Col
-              lg={10}
-              style={{
-                backgroundColor: " #e0ded8 ",
-                height: "35rem",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-between",
-                overflow: "hidden",
-              }}
-            >
-              <Header />
+                <Col
+                  lg={10}
+                  style={{
+                    backgroundColor: " #e0ded8 ",
+                    height: "35rem",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                    overflow: "hidden",
+                  }}
+                >
+                  <Header />
 
-              <TodoList />
-              <TodoInput />
-            </Col>
-          </>
-        )}
-      </Row>
-    </Container>
+                  <TodoList />
+                  <TodoInput />
+                </Col>
+              </>
+            )}
+          </Row>
+        </Container>
+      ) : (
+        <h1>error</h1>
+      )}
+    </>
   );
 };
 
